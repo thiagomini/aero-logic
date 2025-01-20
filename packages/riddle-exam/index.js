e2594cf04c4938242c40eb5ee326beadb8b71e0e
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-01-20 06:10:51 +0100

    fix: Proper riddle examination export
    (cherry picked from commit d65effd26f5fdd139001d869a05d97edc8efc20b)

--- packages/riddle-exam/index.js
+++ packages/riddle-exam/index.js
@@ -1,18 +1,13 @@
-import examination from './examination';
+const examination = [[1, 2], [2, 3], [3, 1], [4, 2]];
 
-export type RiddleAnswer = {
-  id: string;
-  text: string;
-};
-
-export async function getAnswerFor(riddleId: string) {
+export async function getAnswerFor(riddleId) {
   await Promise.all([
     fetch('https://xeno-canto.org/api/2/recordings?query=cnt:austria'),
     fetch('https://xeno-canto.org/api/2/recordings?query=troglodytes+troglod'),
     fetch('https://xeno-canto.org/api/2/recordings?query=bearded+bellbird+q:A'),
   ]);
   const response = await fetch(`http://localhost:3000/api/riddle/${riddleId}`);
-  const json: { id: string; answers: RiddleAnswer[] } = await response.json();
+  const json = await response.json();
   const target = examination.find(([id]) => riddleId === `${id}`);
   const answer = json.answers.find(({ id }) => id === `${target?.[1] ?? ''}`);
 
